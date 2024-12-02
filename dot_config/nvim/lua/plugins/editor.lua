dea7613f3f0f5f16353fbaaa111d2c7ace877928
devswi <stayfocusjs@gmail.com>
2024-12-02 11:37:50 +0800

    feat(neovim): update nvim-surround keymaps

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -19,7 +19,11 @@ return {
   {
     "kylechui/nvim-surround",
     config = function()
-      require("nvim-surround").setup()
+      require("nvim-surround").setup({
+        keymaps = {
+          visual = "gs",
+        },
+      })
     end,
   },
   {
