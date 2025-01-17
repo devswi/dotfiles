3da38216ad5c1c0a038474c2658c7fc8893c94ab
devswi <stayfocusjs@gmail.com>
2025-01-17 10:06:19 +0800

    chore(neovim): bump version

--- dot_config/nvim/lua/plugins/extras/coding/autopairs.lua
+++ dot_config/nvim/lua/plugins/extras/coding/autopairs.lua
@@ -17,7 +17,7 @@ return {
       },
       fast_wrap = {
         map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
+        chars = { "{", "[", "(", '"', "'", "<" },
         pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
         offset = 0, -- Offset from pattern match
         end_key = "$",
